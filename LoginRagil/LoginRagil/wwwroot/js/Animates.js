cbda8bf941f69dae8d763160da3c68dec721950b
IamShaharFar <shaharfar16@gmail.com>
2023-03-20 22:34:55 +0200

    mooving btn

---
+++ LoginRagil/LoginRagil/wwwroot/js/Animates.js
@@ -0,0 +1,12 @@
+﻿const form = document.querySelector('form');
+const registerBtn = document.getElementById('registerBtn');
+
+form.addEventListener('submit', function (event) {
+    if (form.checkValidity()) {
+        registerBtn.classList.add('animate__shakeX');
+
+        registerBtn.addEventListener('animationend', function () {
+            registerBtn.classList.remove('animate__shakeX');
+        }, { once: true });
+    }
+});
