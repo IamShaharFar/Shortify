e26706e00a91cca86991c1551bc30bcae40bad4b
IamShaharFar <shaharfar16@gmail.com>
2023-03-21 02:26:34 +0200

    .

--- LoginRagil/LoginRagil/wwwroot/js/Animates.js
+++ LoginRagil/LoginRagil/wwwroot/js/Animates.js
@@ -2,7 +2,8 @@
 const registerBtn = document.getElementById('registerBtn');
 
 form.addEventListener('submit', function (event) {
-    if (form.checkValidity()) {
+    let isvalid = await isValidUser(getuser());
+    if (!isvalid) {
         registerBtn.classList.add('animate__shakeX');
 
         registerBtn.addEventListener('animationend', function () {
@@ -10,3 +11,58 @@ form.addEventListener('submit', function (event) {
         }, { once: true });
     }
 });
+
+async function getuser() {
+    const nameInput = document.querySelector('#nameInput').value;
+    const emailInput = document.querySelector('#emailInput').value;
+    const passwordInput = document.querySelector('#passwordInput').value;
+    const confirmPasswordInput = document.querySelector('#confirmPasswordInput').value;
+    const agreeServicesCheckbox = document.querySelector('#AgreeServices').value;
+
+    const user = {
+        Name: nameInput,
+        Email: emailInput,
+        Password: passwordInput,
+        ConfirmPassword: confirmPasswordInput,
+        AgreeServices: agreeServicesCheckbox
+    };
+}
+
+function isValidUser(model) {
+    if (!model) {
+        return false;
+    }
+    // Check if all required fields are present
+    if (!model.Name || !model.Email || !model.Password || !model.ConfirmPassword || model.AgreeServices === false) {
+        return false;
+    }
+
+    // Check if the Name field is between 2 and 20 characters
+    if (model.Name.length < 2 || model.Name.length > 20) {
+        return false;
+    }
+
+    // Check if the Email field is a valid email address
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(model.Email)) {
+        return false;
+    }
+
+    // Check if the Password field is between 4 and 20 characters
+    if (model.Password.length < 4 || model.Password.length > 20) {
+        return false;
+    }
+
+    // Check if the ConfirmPassword field matches the Password field
+    if (model.ConfirmPassword !== model.Password) {
+        return false;
+    }
+
+    // Check if the AgreeServices field is true
+    if (!model.AgreeServices) {
+        return false;
+    }
+
+    // If all checks pass, the user is considered valid
+    return true;
+}
